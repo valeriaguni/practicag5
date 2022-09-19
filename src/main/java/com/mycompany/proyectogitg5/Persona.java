bae366340b630479c203f3732e75a2906007f9d5
valeria gomez <valeria.gomez.mt@usa.edu.co>
2022-09-19 10:19:42 -0600

    agregando clase Persona.java

---
+++ src/main/java/com/mycompany/proyectogitg5/Persona.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.proyectogitg5;
+
+/**
+ *
+ * @author vival
+ */
+public class Persona {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola a todos");
+        System.out.println("Hola");
+        int x=4;
+        double y=3;
+    }
+    
+    
+}
